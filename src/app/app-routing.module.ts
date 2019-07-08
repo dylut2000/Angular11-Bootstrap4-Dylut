5338319e597c8b6a0ad096f322ff199d5e6b8b57
Hardy <hardy@thedigitalacademy.co.za>
2019-07-08 12:58:33 +0200

    everything set for you, but im using the bootstrap-bundles instead of popper.js, u can modify it back to popper.js in the angular.json

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-
-const routes: Routes = [];
+// this constant default routes to Home, and also send all unknown path to Home
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
